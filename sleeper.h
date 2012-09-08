df351ed72de6b780350f9528f0f434928eddcc14
Kiril Mikos <kiril.mik.os@gmail.com>
2012-09-08 12:52:24 +0300

    Sleeper: Add checking for negative value

--- sleeper.h
+++ sleeper.h
@@ -1,9 +1,25 @@
 #ifndef SLEEPER_H
 #define SLEEPER_H
 
+#include <QThread>
+
 class Sleeper : public QThread {
 public:
-    static void msleep(unsigned long v) { QThread::msleep(v); }
+    static void msleep(long v) {
+        if (v > 0) {
+            QThread::msleep(v);
+        }
+    }
+    static void sleep(long v) {
+        if (v > 0) {
+            QThread::sleep(v);
+        }
+    }
+    static void usleep(long v) {
+        if (v > 0) {
+            QThread::usleep(v);
+        }
+    }
 };
 
 #endif // SLEEPER_H
